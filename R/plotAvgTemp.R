f18067dc14ad56345b1a2b1a4f4f55fd0850943d
Giuseppe Carlino <g.carlino@simularia.it>
2014-04-11 15:23:38 +0200

    data table in plotAvgTemp

--- R/plotAvgTemp.R
+++ R/plotAvgTemp.R
@@ -1,13 +1,14 @@
 #' Plot average temperature
 #' 
-#' \code{plotAvgTemp} builds a plot with histograms of monthly average temperature and two lines with maximim and minimum monthly temperature.
+#' \code{plotAvgTemp} builds a bar plot of time average temperature and two line plots with maximum and minimum temperature.
 #'  
 #' @param mydata A data frame containing fields date and temp
 #' @param temp   Name of the column representing temperature
 #' @param avg.time This defines the time period to average to (see openair::timeAverage). Default is "1 month".
 #' @param ylabel The label to be plot along y axis
+#' @param title Option plot title
 #' 
-#' @return A ggplot2 plot object.
+#' @return A plot with avereage, min and max temperature in a given range of time.
 #' 
 #' @note \code{plotAvgTemp} uses \code{openair::timeAvearge} to compute average.
 #' 
@@ -17,32 +18,85 @@
 #' 
 #' # Plot histogram with monthly averages together with maxima and minima curves
 #' plotAvgTemp(mydata)
+#' plotAvgTemp(mydata, temp="temperature", avg.time="1 month", ylabel="Temperatura [C]")
 #' 
-plotAvgTemp <- function(mydata, temp="temp", avg.time="1 month", ylabel="Temperatura [°C]") {
+plotAvgTemp <- function(mydata, temp = "temp", 
+                        avg.time = "1 month", 
+                        ylabel = "Temperatura [°C]",
+                        title = "") {
     require("openair")
     require("ggplot2")
     require("scales")
+    require("reshape2")
+    require("grid")
     
-    mydata_mean <- timeAverage(mydata, statistic="mean", avg.time=avg.time)
-    mydata_max <- timeAverage(mydata, statistic="max", avg.time=avg.time)
-    mydata_min <- timeAverage(mydata, statistic="min", avg.time=avg.time)
-    mydata_mean <- merge(mydata_mean, mydata_min, by="date", all=T)
-    mydata_mean <- merge(mydata_mean, mydata_max, by="date", all=T)
-    mydata_mean <- subset(mydata_mean, select=c("date", "temp.x", "temp.y", "temp"))
+    mydata_mean <- timeAverage(mydata, statistic = "mean", avg.time = avg.time)
+    mydata_max <- timeAverage(mydata, statistic = "max", avg.time = avg.time)
+    mydata_min <- timeAverage(mydata, statistic = "min", avg.time = avg.time)
+    mydata_mean <- merge(mydata_mean, mydata_min, by = "date", all = T)
+    mydata_mean <- merge(mydata_mean, mydata_max, by = "date", all = T)
+    mydata_mean <- subset(mydata_mean, 
+                          select = c("date", "temp.x", "temp.y", "temp"))
     colnames(mydata_mean) <- c("date", "temp", "temp.min", "temp.max")
     # mydata_mean$month <- months.POSIXt(mydata_mean$date)
     # mydata_mean$month <- factor(mydata_mean$month, levels=month.name)
     
-    v <- ggplot(data=mydata_mean, aes(date, temp)) + 
-        geom_histogram(aes(date, temp, color="Media", fill="Media"), stat="identity", show_guide=FALSE) + 
-        geom_line(aes(x=date, y=temp.min, color="Minimo"), size=1) + 
-        geom_line(aes(date, temp.max, color="Massimo"), size=1) + 
-        scale_y_continuous(breaks=seq(-20,40,5)) + labs(x="", y=ylabel) +
-        scale_x_datetime(breaks=date_breaks(width=avg.time), labels = date_format("%b")) +
-        scale_color_manual(values=c("Media"="steelblue", "Minimo"="darkgreen","Massimo"="darkorange2"), guide=guide_legend(title=NULL)) +
-        scale_fill_manual(values=c("Media"="steelblue"), guide=F)  + 
-        theme_bw(base_family="Helvetica") +
-        theme(legend.position=c(0,1), legend.justification=c(0,1))
- 
-    return(v)
+    v <- ggplot(data = mydata_mean, aes(date, temp)) + 
+        geom_histogram(aes(date, temp, color = "Media", fill = "Media"), 
+                       stat = "identity", 
+                       show_guide = FALSE) + 
+        geom_line(aes(x = date, y = temp.min, color = "Minima"), size = 1) + 
+        geom_line(aes(date, temp.max, color = "Massima"), size = 1) + 
+        scale_y_continuous(labels  = math_format(.x * degree), 
+            breaks = seq(-20, 40, 5)) + 
+        labs(title = title, x = "", y = ylabel) +
+        scale_x_datetime(breaks = date_breaks(width = avg.time), labels = date_format("%b")) +
+        scale_color_manual(values = c("Media" = "steelblue", 
+                                      "Minima" = "darkgreen", 
+                                      "Massima" = "darkorange2"), 
+                           guide=guide_legend(title = NULL)) +
+        scale_fill_manual(values = c("Media" = "steelblue"), guide = F)  + 
+        theme_bw(base_family = "Helvetica") +
+        theme(legend.position = c(0, 1), legend.justification = c(0, 1))
+    
+    # Prepare table of data to be plot in the lower part of the figure
+    # See http://learnr.wordpress.com/2009/04/29/
+    #                   ggplot2-labelling-data-series-and-adding-a-data-table/
+    
+    mydata <- melt(mydata_mean, measure.vars = c("temp.min", "temp", "temp.max"))
+    mydata$value <- round(mydata$value, digits = 1)
+    data_table <- ggplot(mydata, 
+                         aes(date, factor(variable), label = format(value, nsmall = 1))) +
+        geom_text(size = 3.5) +
+        scale_y_discrete(labels = c("min", "media", "max")) +
+        theme_bw() + 
+        labs(title = NULL, x = NULL, y = NULL) +
+        theme(
+            plot.margin = unit(c(-0.5, 2, 0, 2), "lines"),
+            axis.text.x = element_blank(),
+            axis.ticks = element_blank(),
+            panel.border = element_blank(),
+            panel.grid = element_blank())
+    
+    mylayout <- grid.layout(nrow = 2, ncol = 1, heights = unit(c(2, 0.25), c("null", "null")))
+    
+#     grid.show.layout(mylayout)
+    vplayout <- function(...) {
+        grid.newpage()
+        pushViewport(viewport(layout = mylayout))
+    }
+    subplot <- function(x, y) viewport(layout.pos.row = x,
+                                       layout.pos.col = y)
+    mmplot <- function(a, b) {
+        vplayout()
+        print(a, vp = subplot(1, 1))
+        print(b, vp = subplot(2, 1))
+    }
+
+#     return( data_table)
+    
+    myplot <- mmplot(v, data_table)
+
+    
+#     return(myplot)
 }
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # simulariatools
 
-simulariatools is a package of functions and tools usefule to pre-process and post-process
-data for air quality assessment.
+`simulariatools` is a collection of functions and tools useful to pre and post process
+data for air quality assessment. It is developed and mantained by the people at Simularia www.simularia.it.
 
-To install:
+You can install it from github repository, through the `devtools` package:
  
     install("devtools")
     install_github("Simularia/simulariatools")
 
+If you are interested, you can track development at http://github.com/simularia/simulariatools
