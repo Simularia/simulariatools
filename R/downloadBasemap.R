dc813217f468b33817c65efba60fb34d3198e65f
Giuseppe Carlino <g.carlino@simularia.it>
2024-03-06 17:55:42 +0100

    Fix url of PCN

--- R/downloadBasemap.R
+++ R/downloadBasemap.R
@@ -1,11 +1,11 @@
-#' Download basemap from Italian National Geoportal 
-#' 
-#' This function tries to download the aerial orthophoto of the requested 
-#' domain from the 
-#' [Italian National Geoportal](http://www.pcn.minambiente.it/mattm/en/services/).
+#' Download basemap from Italian National Geoportal
+#'
+#' This function tries to download the aerial orthophoto of the requested
+#' domain from the
+#' [Italian National Geoportal](https://gn.mase.gov.it/portale/home).
 #' The output is given in *png* format at the path given in the `file`
 #' parameter.
-#' 
+#'
 #' @param file Path to output file.
 #' @param xSW South West Easting UTM coordinate of the basemap (in metres).
 #' @param ySW South West Northing UTM coordinate of the basemap (in metres).
@@ -14,41 +14,41 @@
 #' @param crs UTM Coordinate Reference System: either 32 or 33.
 #' @param width The basemap width.
 #' @param height The basemap height.
-#' @param units The unit of measure of width and height. 
+#' @param units The unit of measure of width and height.
 #'             It can be `px` (pixels, the default), `in` (inches), `cm` or `mm`
-#' @param res The resolution in dpi. 
-#' 
+#' @param res The resolution in dpi.
+#'
 #' @return No value is returned.
-#' 
+#'
 #' @export
 #' @importFrom utils download.file
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{
-#' # Download a basemap of a domain with SW coordinates (410000, 5000500) 
+#' # Download a basemap of a domain with SW coordinates (410000, 5000500)
 #' # in the UTM32 CRS and extension 5000m in both directions.
-#' 
-#' downloadBasemap(file = "./basemap.png", 
+#'
+#' downloadBasemap(file = "./basemap.png",
 #'                 xSW = 410000, ySW = 5000500, xExt = 5000, yExt = 5000)
 #'
-#' # Download a basemap of a domain with SW coordinates (410000, 5000500) 
+#' # Download a basemap of a domain with SW coordinates (410000, 5000500)
 #' # in the UTM32 CRS and extension 5000m in both directions.
 #' # The file has to be 2048 x 2048 pixels.
-#' 
-#' downloadBasemap(file = "./basemap.png", 
-#'                 xSW = 410000, ySW = 5000500, xExt = 5000, yExt = 5000, 
+#'
+#' downloadBasemap(file = "./basemap.png",
+#'                 xSW = 410000, ySW = 5000500, xExt = 5000, yExt = 5000,
 #'                 width = 2048, height = 2048)
-#'                 
+#'
 #' # Download a basemap of a domain with SW coordinates (410000, 5000500)
 #' # in the UTM32 CRS and extension 5000m in both directions.
 #' # The file has to be 10cm x 10cm with a resolution of 150 dpi.
-#' 
-#' downloadBasemap(file = "./basemap.png", 
-#'                 xSW = 410000, ySW = 5000500, xExt = 5000, yExt = 5000, 
+#'
+#' downloadBasemap(file = "./basemap.png",
+#'                 xSW = 410000, ySW = 5000500, xExt = 5000, yExt = 5000,
 #'                 width = 10, height = 10, units = "cm", res = 150)
 #' }
 downloadBasemap <- function(file = file,
-                            xSW = 410000, ySW = 5000500, 
+                            xSW = 410000, ySW = 5000500,
                             xExt = 5000, yExt = 5000,
                             crs = 32,
                             width = 1024,
@@ -60,13 +60,13 @@ downloadBasemap <- function(file = file,
     if (missing(file)) {
         stop("Please define the output file.", call. = FALSE)
     }
-    
+
     # Trying to access PCN
     message("Trying to access http://wms.pcn.minambiente.it")
-    
+
     # Indirizzo base per WMS ortofoto 2012 da PCN
     url1 <- "http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/raster/ortofoto_colore_12.map&"
-    
+
     # File size
     if (units == "in") {
         width <- width * res
@@ -85,7 +85,7 @@ downloadBasemap <- function(file = file,
     }
     width <- round(width, digits = 0)
     height <- round(height, digits = 0)
-    
+
     # CRS
     if (crs == 32) {
         # "EPSG:32632" UTM 32
@@ -96,19 +96,19 @@ downloadBasemap <- function(file = file,
     } else {
         stop("CRS not recognized.", call. = FALSE)
     }
-    
+
     # FORMAT
     format <- "png"
-    
+
     url2 <- paste("VERSION=1.3.0&REQUEST=GetMap&LAYERS=OI.ORTOIMMAGINI.2012&STYLES=default&CRS=",
                   crsStr, "&BBOX=", xSW, ",", ySW, ",", xSW + xExt, ",",
                   ySW + yExt, "&WIDTH=", width, "&HEIGHT=",
                   height, "&FORMAT=image/",
                   format, sep = "")
     url <- paste(url1, url2, sep = "")
-    
+
     # DL png file with specific CRS, BBOX, WIDTH, HEIGHT and FORMAT
     download.file(url, destfile = file, quiet = TRUE)
-    
+
     message(paste("Please check for output file at:", file))
 }
